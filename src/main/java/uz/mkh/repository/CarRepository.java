c67785b3597fdf66ceadc9edf5ae285f4b23a837
mukhammadjonkholmukhamedov <kh.muxammadjon2@gmail.com>
2025-11-01 13:24:09 +0500

    fix: moved search to sql level

--- src/main/java/uz/mkh/repository/CarRepository.java
+++ src/main/java/uz/mkh/repository/CarRepository.java
@@ -1,6 +1,7 @@
 package uz.mkh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uz.mkh.model.entity.CarEntity;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
 
     List<CarEntity> findCarByOwnerIdIs(Long id);
+
+    @Query("select count(distinct function('split_part',c.model,'-',1)) from CarEntity c")
+    Long countDistinctVendor();
 }
